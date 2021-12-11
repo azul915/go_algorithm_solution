4537aa4c08731d2f932c3b8c81d9526441a13e3a
azul915 <k.aoyagi940516@gmail.com>
2021-12-12 03:32:12 +0900

    add SearchAgeOfA

--- chap1/questions.go
+++ chap1/questions.go
@@ -1,9 +1,36 @@
 package chap1
 
+import (
+	"fmt"
+)
+
 func ResponseFromChap1() string {
 	return "This is response from chap1"
 }
 
 func hoge() string {
 	return "hoge"
+}
+
+// Aさんの年齢が22歳、範囲が20歳以上36歳未満だとする
+func SearchAgeOfA() int {
+
+	targetAge := 22
+	left := 20
+	right := 35
+
+	for left <= right {
+		mid := left + ((right - left) / 2)
+		if mid > targetAge {
+			right = mid - 1
+			fmt.Printf("left: %v, mid: %v right: %v\n", left, mid, right)
+		} else if mid == targetAge {
+			fmt.Printf("left: %v, mid: %v right: %v\n", left, mid, right)
+			return mid
+		} else { // mid < targetAge
+			left = mid + 1
+			fmt.Printf("left: %v, mid: %v right: %v\n", left, mid, right)
+		}
+	}
+	return -1
 }
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/azul915/go_algorithm_solution/chap1"
 )
 
-func main () {
+func main() {
 
-	fmt.Println("This is go_algorithm_solution")
-	fmt.Println(chap1.ResponseFromChap1())
+	fmt.Println(chap1.SearchAgeOfA())
 }
