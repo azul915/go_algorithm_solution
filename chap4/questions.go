fedfb8ee3c746787f5e33f5964518dacc4a42d49
azul915 <k.aoyagi940516@gmail.com>
2022-01-19 01:19:11 +0900

    add 4.2

--- chap4/questions.go
+++ chap4/questions.go
@@ -22,8 +22,48 @@ func trib(n int) int {
 
 func Question4_1() {
 	// 第0項~第10項を求める
-	for N := 0; N < 10; N++ {
+	k := 10
+	for N := 0; N < k; N++ {
 		fmt.Printf("%d, ", trib(N))
+	}
+	fmt.Println("...")
+}
+
+// p.542
+// 4.2
+// 4.1をメモ化によって効率化する
+// 計算量 O(N)
+var m [10]int
+
+func tribWithMemo(n int) int {
+
+	if n == 0 || n == 1 {
+		return 0
+	}
+
+	if n == 2 {
+		return 1
+	}
+
+	if m[n] != -1 {
+		return m[n]
+	}
+
+	m[n] = tribWithMemo(n-1) + tribWithMemo(n-2) + tribWithMemo(n-3)
+	return m[n]
+}
+
+func Question4_2() {
+
+	k := 10
+	m[0], m[1], m[2] = 0, 0, 1
+	// メモ用配列init
+	for N := 3; N < k; N++ {
+		m[N] = -1
+	}
+
+	for N := 0; N < k; N++ {
+		fmt.Printf("%d, ", tribWithMemo(N))
 	}
 	fmt.Println("...")
 }
--- main.go
+++ main.go
@@ -11,5 +11,5 @@ import (
 
 func main() {
 
-	chap4.Question4_1()
+	chap4.Question4_2()
 }
